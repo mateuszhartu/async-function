8a7e25502c56d410e9fea04ded3cef7ac1b6334a
mateuszhartu <32650323+mateuszhartu@users.noreply.github.com>
2019-03-17 09:25:17 +0100

    Add files via upload

---
+++ async function/test.js
@@ -0,0 +1,54 @@
+
+async function start(){
+    let users = await dbUsers();
+    let companies = await dbCompanies();
+    let data = document.getElementById('finalTable');
+
+    console.log(companies);
+    console.log(users);
+
+    for(let i=0; i<companies.length; i++){
+
+        let row = data.insertRow(i+1);
+        let comp = row.insertCell(0);
+        let usr = row.insertCell(1);
+
+        comp.innerText = companies.filter(function (el) {
+            return el.uri === `/companies/${i}`;
+        }).map(a => a.name);
+        usr.innerText = users.filter(function (el) {
+            return el.uris.company === `/companies/${i}`;
+        }).map(a => a.name);
+
+    }
+
+}
+
+
+    async function dbUsers() {
+        let users;
+
+        const usersData = fetch(`http://localhost:3000/users`)
+            .then(res => res.json());
+
+
+        await usersData.then(res => users = res); // pamiętaj o = res!! dlatego nie działąło
+
+        return users;
+
+    }
+
+    async function dbCompanies() {
+        let companies;
+
+        const companiesData = fetch(`http://localhost:3000/companies`)
+            .then(res => res.json());
+
+
+        await companiesData.then(res => companies = res); // pamiętaj o = res!! dlatego nie działąło
+
+        return companies;
+
+    }
+
+    start();
